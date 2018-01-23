3978ad4a72d4354b5426ca4590bb2c80cfe24418
xiaolin <462441355@qq.com>
2018-01-23 21:58:02 +0800

    * 引入第三方js

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - style-loader-less-sass(处理less、sass)
 - extract-text-webpack-plugin(提取css)
 - post-css(代码补全做到浏览器兼容)
+- provide-plugin(引入第三方 例如jquery)
---
+++ provide-plugin/src/app.js
@@ -0,0 +1,8 @@
+import base from './css/base.less'
+import './css/common.less'
+
+var app = document.getElementById('app')
+app.innerHTML = '<div class="' + base.box + '"></div>'
+
+console.log($)
+
