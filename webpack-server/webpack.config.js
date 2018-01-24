455e89c5b410060247a9b1b1692bd093a5e7336b
xiaolin <462441355@qq.com>
2018-01-24 23:03:12 +0800

    请求可以设置header

--- webpack-server/webpack.config.js
+++ webpack-server/webpack.config.js
@@ -24,7 +24,11 @@ module.exports = {
             '/rest':{
                 target: 'http://www.system.com',
                 changeOrigin: true,
-                logLevel: 'debug' // 开启debug模式  可以在控制台看到设置那些代理
+                logLevel: 'debug', // 开启debug模式  可以在控制台看到设置那些代理
+                // 设置请求的header
+                headers: {
+
+                }
             }
         },
         // 单页面应用路由 使用#请求的是某一个页面 使用historyApiFallback不造成浏览器刷新 直接改变history历史
