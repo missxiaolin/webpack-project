614b36a2cb5d22b313c3021c35c7f4d0db216169
xiaolin <462441355@qq.com>
2018-01-27 01:49:20 +0800

    * 代码规范

--- webpack-server/build/webpack.dev.conf.js
+++ webpack-server/build/webpack.dev.conf.js
@@ -21,6 +21,6 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
 
-        new webpack.NamedModulesPlugin(),
+        new webpack.NamedModulesPlugin()
     ]
 }
