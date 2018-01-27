e19a4ae9888d88092c1928c1a61822f001f5cf88
xiaolin <462441355@qq.com>
2018-01-27 20:56:25 +0800

    * 添加react 和 css 和 js

---
+++ webpack-server-more/src/pages/a.js
@@ -0,0 +1,5 @@
+import react from 'react'
+import moduleA from '../components/module'
+
+console.log('this is a')
+console.log(moduleA)
---
+++ webpack-server-more/src/pages/c.js
@@ -0,0 +1,5 @@
+import react from 'react'
+import moduleA from '../components/module'
+
+console.log('this is c')
+console.log(moduleA)
